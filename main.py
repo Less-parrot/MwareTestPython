b8c6db17a0741d2463dd717fb5ed31841c895f7f
Less-parrot <less100100@gmail.com>
2024-02-08 22:52:00 -0500

    ui/cli independientes

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
+from os import system
 
-
+class Cli:
+    
+    def Options():
+        ask = str(input("Escoja el entirno que desea utilizar [ui/cli]: "))
+        if ask == "ui":
+            system("cd ui;clear;./bins/UiMainFlet; cd ..")
+        elif ask == "cli":
+            system("clear")
+            print("Bienvenido a la cli")
+            exit() 
+    
+Cli.Options()
